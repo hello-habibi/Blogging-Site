57ef143ddf2aca3d0c1215bd12b65422290e8045
Habibur Rahman <hr13338@gmail.com>
2024-01-17 07:32:41 +0600

    appWrite is added as a backend service and authentication service provider

---
+++ src/conf/Conf.js
@@ -0,0 +1,12 @@
+const conf = {
+
+    appWriteUrl : String(import.meta.env.VITE_APPWITE_LINK),
+    projectID : String(import.meta.env.VITE_APPWITE_LINK),
+    databaseID : String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    collectionID : String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    bucketID : String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+
+
+}
+
+export default conf;
